aa1a34ce2dc5e0d45efcb773b6772a0fa4b95093
inochidainini <aki.sara.ima@gmail.com>
2026-02-18 03:10:40 +0900

    効果編集

--- evolveCardEditor/ui.js
+++ evolveCardEditor/ui.js
@@ -18,7 +18,7 @@ export function bindEvents(handlers) {
         document.getElementById('effect-modal').classList.remove('show');
     })
     document.getElementById("btn-save-effect").addEventListener("click", () => {
-        const custom_effect = document.getElementById('effect-editable').value;
+        const custom_effect = document.getElementById('effect-editable').innerHTML;
         onBtnSaveEffect(custom_effect);
         document.getElementById('effect-modal').classList.remove('show');
     })
@@ -160,7 +160,7 @@ export function renderEffectList(currentDeck, cardDict, handlers) {
             onBtnEdit(cardId);
 
             const content = document.getElementById('effect-editable');
-            content.value = card.custom_effect || card.ability;
+            content.innerHTML = card.custom_effect || card.ability;
 
             document.getElementById('effect-modal').classList.add('show');
             console.log("test")
