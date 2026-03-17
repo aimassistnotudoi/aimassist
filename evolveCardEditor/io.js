54fa7f6ca289a0403dffa05306e370a16a74287c
inochidainini <aki.sara.ima@gmail.com>
2026-03-18 01:19:39 +0900

    デッキ保存

--- evolveCardEditor/io.js
+++ evolveCardEditor/io.js
@@ -1,21 +1,37 @@
 export async function loadCardJson() {
     try {
-      const cardsRes = await fetch('cards.json');
-      const uniqueRes = await fetch('unique_cards.json');
+        const cardsRes = await fetch('cards.json');
+        const uniqueRes = await fetch('unique_cards.json');
 
-      const cards = await cardsRes.json();
-      const uniqueCards = await uniqueRes.json();
+        const cards = await cardsRes.json();
+        const uniqueCards = await uniqueRes.json();
 
-      return [cards, uniqueCards];
+        return [cards, uniqueCards];
     } catch (error) {
-      console.error('Error loading card JSON:', error);
-      return [[], []];
+        console.error('Error loading card JSON:', error);
+        return [[], []];
     }
 }
 
-// export async function loadDeckJson(deck) {
-//     try{
-//         const response = await fetch(deck);
-//         return await response.json();
-//     }
-// }
+export async function importDeckJson(deckFile) {
+    try{
+        return JSON.parse(await deckFile.text());
+    }
+    catch(error){
+        console.error('Error parsing deck JSON:', error);
+        throw error;
+    }
+}
+
+export function exportDeckJson(deckData) {
+    try{
+        const json = JSON.stringify(deckData, null, 2);
+        const blob = new Blob([json], {type: 'application/json'});
+        const url = URL.createObjectURL(blob);
+        return url;
+    }
+    catch(error){
+        console.error('Error exporting deck JSON:', error);
+        throw error;
+    }
+}
