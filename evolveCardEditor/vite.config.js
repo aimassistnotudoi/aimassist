fa6c3ed48cef14585b446a3c1c045441f7328072
inochidainini <aki.sara.ima@gmail.com>
2026-09-21 00:24:02 +0900

    全ファイルがbuildされる用

--- evolveCardEditor/vite.config.js
+++ evolveCardEditor/vite.config.js
@@ -1,5 +1,24 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { glob } from "glob";
+
+const htmlFiles = glob.sync("**/*.html", {
+    cwd: __dirname,
+    ignore: ["node_modules/**", "dist/**"],
+});
+
+const input = Object.fromEntries(
+    htmlFiles.map((file) => [
+        file.replace(/\.html$/, ""),
+        resolve(__dirname, file),
+    ])
+);
 
 export default defineConfig({
     base: "/aimassist/evolveCardEditor/",
+    build: {
+        rollupOptions: {
+            input,
+        },
+    },
 });
