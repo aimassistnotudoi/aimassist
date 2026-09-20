4727da71f16a8f6480d1e6300791f124e8ce3bd8
inochidainini <aki.sara.ima@gmail.com>
2026-09-20 23:30:36 +0900

    テスト

--- evolveCardEditor/main.js
+++ evolveCardEditor/main.js
@@ -1,3 +1,20 @@
+import { supabase } from "./supabase.js";
+
+async function testSupabase() {
+    const { data, error } = await supabase
+        .from("test")
+        .select("*");
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    console.log(data);
+}
+
+testSupabase();
+
 document.getElementById("deck-button").addEventListener("click", () => {
     const clan = document.querySelector('input[name="clan"]:checked');
 
